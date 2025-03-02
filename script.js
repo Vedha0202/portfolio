d090b0684f2b18c61b71f72afba28609a2094568
Vedha <136982171+Vedha0202@users.noreply.github.com>
2025-03-02 15:31:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+ScrollReveal({
+     reset: true,
+     distance: '80px',
+     duration: 2000,
+     delay: 100
+    
+    });
+
+    ScrollReveal().reveal('.home-content , .head', { origin: 'top' });
